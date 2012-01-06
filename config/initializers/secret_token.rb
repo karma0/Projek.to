57f470a7d1d2594aae36539e156fd90f2ec40b5f
= <=>
2012-01-06 09:41:18 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ombudsman::Application.config.secret_token = '12dd214f4d172b5b05e5327cd3468b7ef342a96b0ef6d6cc183d874307abb849281c06fc6ca078743e60dbb9c635a474e1f5824c0de9acedfc6b1b57ed0e58c7'
