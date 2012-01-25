f0ea162d78372473d71db036659d46faad17ad85
Bobby Larson <karma0@gmail.com>
2012-01-25 13:49:42 +0000

    Fixing up some minor changes with rails 3.1 upgrade.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ Projekto::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.assets.js_compressor = :uglifier
 end
